82370c604b5affeb819ae0d2a5412aab6cb032f0
Haervwe <juan-jose-007@hotmail.com>
2022-07-03 18:29:15 -0300

    add funcions to select game type and create players

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 
-var board = document.getElementById("board");
-
-
 const BoardFactory = (() => {
+    var board = document.getElementById("board");
     var game = [];
     var players = []
     var currentPlayer;
-    const PlayerFactory = (name, type, mark) =>{
+    var gameType;
+
+    function setGameType (type){
+        if (type == "ai"){
+            gameType = "ai"
+        } else {
+            gameType = "twoPlayers"
+        }
+    }
+    
+    const PlayerFactory = (name,type) =>{
+        let mark;
+        if (players.length > 1){
+            return;
+        }
+        if (players.length == 0 ){
+            mark = "X"
+        } else {
+            mark = "O"
+        }
         return {
             name,
             type,
@@ -14,9 +31,24 @@ const BoardFactory = (() => {
         }
     }
 
-    players.push(PlayerFactory("test","human","X"));
-    players.push(PlayerFactory("test2","human","O"));
-    currentPlayer = players[0];
+    function newPlayers (){
+        let form = document.getElementById("playerSelection");
+        let player1name = document.getElementById("player1name");
+        let player2name = document.getElementById("player2name");
+        if (gameType == "ai"){
+            players.push(PlayerFactory(`${form.player.value}`,"human"));
+            players.push(PlayerFactory(`AI`,"ai"))
+            currentPlayer = players[0];
+            
+        } else {
+            players.push(PlayerFactory(`${form.player1.value}`,"human"));
+            players.push(PlayerFactory(`${form.player2.value}`,"human"));
+            currentPlayer = players[0];
+        }
+        player1name.innerText = `${players[0].name}`;
+        player2name.innerText = `${players[1].name}`;
+    }
+
     const SquareFactory = (id) =>{
         
         function render(a) {
@@ -37,13 +69,17 @@ const BoardFactory = (() => {
 
         function setMark (){
             if (this.mark == ""){
-                this.mark = currentPlayer.mark;
-                if (currentPlayer == players[0]){
-                    currentPlayer = players[1];
+                if  (currentPlayer == null){
+                    render(this)
                 } else {
-                    currentPlayer = players[0];
+                    this.mark = currentPlayer.mark;
+                    if (currentPlayer == players[0]){
+                        currentPlayer = players[1];
+                    } else {
+                        currentPlayer = players[0];
+                    }
+                    render(this);
                 }
-                render(this);
             }
         }
         return {
@@ -62,8 +98,36 @@ const BoardFactory = (() => {
 
     return {
         game,
+        players,
+        setGameType,
+        newPlayers,
     }
 
 })();
 
 
+function displayTwoPlayers () {
+    const gameType = document.querySelector(".gameType");
+    const twoPNames = document.querySelector(".twoPNames");
+    gameType.style.display = "none";
+    twoPNames.style.display = "grid";
+    BoardFactory.setGameType("two");
+}
+
+function displayOnePlayer () {
+    const gameType = document.querySelector(".gameType");
+    const onePName = document.querySelector(".onePName");
+    gameType.style.display = "none";
+    onePName.style.display = "grid"
+    BoardFactory.setGameType("ai");
+}
+
+function displayGame () {
+    const gameBoard = document.querySelector(".gameBoard");
+    const onePName = document.querySelector(".onePName");
+    const twoPNames = document.querySelector(".twoPNames");
+    gameBoard.style.display = "Grid";
+    onePName.style.display = "none"
+    twoPNames.style.display = "none"
+    BoardFactory.newPlayers();
+}
